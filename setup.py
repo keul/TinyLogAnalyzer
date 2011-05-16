01fac0a74fa5b033d78a7d0a081434570b8a5ad9
keul <luca@keul.it>
2011-05-16 08:11:52 +0000

    * added "Elapsed time" report

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys, os
 setup(name='TinyLogAnalyzer',
       # py_modules=['tinylogan',],
       scripts=['src/tinylogan',],
-      version="0.2.2",
+      version="0.3.0",
       description="Simple bash utility for analyse HTTP access log with enabled response time",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
